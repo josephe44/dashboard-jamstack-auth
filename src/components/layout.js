885a469e00c469cbc4210a49a236461fac28563e
Osuohia Emmanuel <josephe442@gmail.com>
2021-07-21 00:03:33 +0100

    added the identity context provider and the build

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
-// import { IdentityContextProvider } from 'react-netlify-identity-widget'
+import { IdentityContextProvider } from 'react-netlify-identity-widget'
 import './layout.css'
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <IdentityContextProvider url="https://dashboard-jamstack-auth.netlify.app">
       <header>
         <Link to="/">JAMStack App</Link>
       </header>
       <main>{children}</main>
-    </>
+    </IdentityContextProvider>
   )
 }
 
